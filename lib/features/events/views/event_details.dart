a1625f25f6c730371acc456820efb3980a7ac132
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-06-27 00:19:36 +0800

    improve ui: event details only

--- lib/features/events/views/event_details.dart
+++ lib/features/events/views/event_details.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:chipin_blogpost/features/authentication/services/auth_service.dart';
 import 'package:chipin_blogpost/features/events/models/attendee_model.dart';
 import 'package:chipin_blogpost/features/events/models/event_model.dart';
 import 'package:chipin_blogpost/features/events/services/event_service.dart';
+import 'package:chipin_blogpost/themes.dart/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class EventDetailsScreen extends StatefulWidget {
   final MyEventModel event;
@@ -38,16 +39,29 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
     }
   }
 
+  List<MyEventModel> allEvents = [];
   String currentUserId = '';
 
   @override
   void initState() {
     super.initState();
+    fetchAllEvents();
     getCurrentUserId();
   }
 
   Future<void> getCurrentUserId() async {
     currentUserId = await AuthService.getCreatorId();
+  }
+
+  Future<void> fetchAllEvents() async {
+    try {
+      List<MyEventModel> events = await EventService.getAllEvents();
+      setState(() {
+        allEvents = events;
+      });
+    } catch (error) {
+      // Handle error
+    }
   }
 
   @override
@@ -57,61 +71,95 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Event Details'),
+        backgroundColor: Palette.primary200,
+        title: const Text(
+          'Event Details',
+          style: TextStyle(
+            color: Palette.neutral0,
+          ),
+        ),
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.event.title,
-              style: const TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Text(
+                      widget.event.title,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      joinEvent(widget.event);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Palette.primary100,
+                    ),
+                    child: const Text(
+                      'Join Event',
+                      style: TextStyle(
+                        color: Palette.neutral0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Table(
-              columnWidths: const {
-                0: FlexColumnWidth(2),
-                1: FlexColumnWidth(3),
-              },
-              border: TableBorder.all(),
-              children: [
-                _buildTableRow(
-                  'Date',
-                  dateFormat.format(widget.event.dateTime),
-                  Icons.calendar_today,
+              const SizedBox(height: 16.0),
+              Table(
+                columnWidths: const {
+                  0: FlexColumnWidth(2),
+                  1: FlexColumnWidth(3),
+                },
+                border: TableBorder.symmetric(
+                  inside: BorderSide.none,
+                  outside: BorderSide.none,
                 ),
-                _buildTableRow(
-                  'Time',
-                  timeFormat.format(widget.event.dateTime),
-                  Icons.access_time,
+                children: [
+                  _buildTableRow(
+                    'Date',
+                    dateFormat.format(widget.event.dateTime),
+                    Icons.calendar_today,
+                  ),
+                  _buildTableRow(
+                    'Time',
+                    timeFormat.format(widget.event.dateTime),
+                    Icons.access_time,
+                  ),
+                  _buildTableRow(
+                    'Location',
+                    widget.event.location,
+                    Icons.location_on,
+                  ),
+                  _buildTableRow(
+                    'Description',
+                    widget.event.description,
+                    Icons.description,
+                  ),
+                  // Add other event details here
+                  // ...
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Center(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
                 ),
-                _buildTableRow(
-                  'Location',
-                  widget.event.location,
-                  Icons.location_on,
-                ),
-                _buildTableRow(
-                  'Description',
-                  widget.event.description,
-                  Icons.description,
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            Image.network(
-              'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () => joinEvent(widget.event),
-              child: const Text('Join Event'),
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -120,17 +168,34 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
   TableRow _buildTableRow(String label, String value, IconData icon) {
     return TableRow(
       children: [
-        TableCell(
+        Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              Icon(icon),
+              Icon(
+                icon,
+                color: Palette.primary100,
+              ),
               const SizedBox(width: 8.0),
-              Text(label),
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Palette.primary100,
+                  fontSize: 18.0,
+                ),
+              ),
             ],
           ),
         ),
-        TableCell(
-          child: Text(value),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            value,
+            style: const TextStyle(
+              color: Palette.neutral70,
+              fontSize: 18.0,
+            ),
+          ),
         ),
       ],
     );
