721dac40351a3ccbdb9159e55f20a849321520d7
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-26 23:45:28 +0800

    add a palette class

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
-import 'package:chipin_blogpost/features/authentication/views/auth_main_view.dart';
+import 'package:chipin_blogpost/features/myapp.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MyApp(),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'chipIn',
-      home: AuthMainView(),
-    );
-  }
 }
